83cdd23f8e673ff34ed94068444f602916699445
Agathe <30318118+AgatheTheCode@users.noreply.github.com>
2022-04-28 17:44:57 +0200

    twig page admin
    quasi bon ; problème de titre

--- include/page-admin-data.php
+++ include/page-admin-data.php
@@ -1,8 +1,30 @@
 <?php
 $categorie =[
-    'titre' =>'Ajouter une catégorie', 
-    'action' =>'<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">',
+    'titre' =>'Ajouter une catégorie', 'formulaire' =>[
+    ['action' =>'<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">', 
     'nom' =>'Nom Catégorie',
     'input' =>'type="text" name="nom_categorie" class="form-control <?php echo (!empty($nom_categorie_err)) ? \'is-invalid\' : \'\'; ?>" value="<?php echo $nom_categorie; ?>"',
-    'invalid_feeback' =>'<?php echo $nom_categorie_err; ?>',
+    'invalid_feeback' =>'$nom_categorie_err; ?>',],
+    ['action' =>'<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">', 
+    'nom' =>'Description Catégorie',
+    'input' =>'type="text" name="description_categorie" class="form-control <?php echo (!empty($nom_categorie_err)) ? \'is-invalid\' : \'\'; ?>" value="<?php echo $description_categorie; ?>"',
+    'invalid_feeback' =>'$description_categorie_err; ?>',],
+    ['action' =>'<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">', 
+    'nom' =>'Image Catégorie',
+    'input' =>'type="text" name="image_categorie" class="form-control <?php echo (!empty($image_categorie_err)) ? \'is-invalid\' : \'\'; ?>" value="<?php echo $image_categorie; ?>"',
+    'invalid_feeback' =>'$image_categorie_err; ?>',]],
+
+    'titre' =>'Ajouter un genre', 'formulaire' =>[
+        ['action' =>'<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">', 
+        'nom' =>'Nom Catégorie',
+        'input' =>'type="text" name="nom_categorie" class="form-control <?php echo (!empty($nom_categorie_err)) ? \'is-invalid\' : \'\'; ?>" value="<?php echo $nom_categorie; ?>"',
+        'invalid_feeback' =>'$nom_categorie_err; ?>',],
+        ['action' =>'<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">', 
+        'nom' =>'Description Catégorie',
+        'input' =>'type="text" name="description_categorie" class="form-control <?php echo (!empty($nom_categorie_err)) ? \'is-invalid\' : \'\'; ?>" value="<?php echo $description_categorie; ?>"',
+        'invalid_feeback' =>'$description_categorie_err; ?>',],
+        ['action' =>'<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">', 
+        'nom' =>'Image Catégorie',
+        'input' =>'type="text" name="image_categorie" class="form-control <?php echo (!empty($image_categorie_err)) ? \'is-invalid\' : \'\'; ?>" value="<?php echo $image_categorie; ?>"',
+        'invalid_feeback' =>'$image_categorie_err; ?>',]],
 ];
