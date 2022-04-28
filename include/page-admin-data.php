5e1037e196a96c8b5432e894d49a962e14ec4d1e
Agathe <30318118+AgatheTheCode@users.noreply.github.com>
2022-04-28 17:52:51 +0200

    Update page-admin-data.php

--- include/page-admin-data.php
+++ include/page-admin-data.php
@@ -1,6 +1,7 @@
 <?php
 $categorie =[
-    'titre' =>'Ajouter une catégorie', 'formulaire' =>[
+    ['titre' =>'Ajouter une catégorie', 'formulaire' => [
+
     ['action' =>'<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">', 
     'nom' =>'Nom Catégorie',
     'input' =>'type="text" name="nom_categorie" class="form-control <?php echo (!empty($nom_categorie_err)) ? \'is-invalid\' : \'\'; ?>" value="<?php echo $nom_categorie; ?>"',
@@ -12,9 +13,11 @@ $categorie =[
     ['action' =>'<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">', 
     'nom' =>'Image Catégorie',
     'input' =>'type="text" name="image_categorie" class="form-control <?php echo (!empty($image_categorie_err)) ? \'is-invalid\' : \'\'; ?>" value="<?php echo $image_categorie; ?>"',
-    'invalid_feeback' =>'$image_categorie_err; ?>',]],
+    'invalid_feeback' =>'$image_categorie_err; ?>'],
+    ] ],
 
-    'titre' =>'Ajouter un genre', 'formulaire' =>[
+    ['titre' =>'Ajouter un genre', 'formulaire' => [
+
         ['action' =>'<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">', 
         'nom' =>'Nom Catégorie',
         'input' =>'type="text" name="nom_categorie" class="form-control <?php echo (!empty($nom_categorie_err)) ? \'is-invalid\' : \'\'; ?>" value="<?php echo $nom_categorie; ?>"',
@@ -26,5 +29,6 @@ $categorie =[
         ['action' =>'<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">', 
         'nom' =>'Image Catégorie',
         'input' =>'type="text" name="image_categorie" class="form-control <?php echo (!empty($image_categorie_err)) ? \'is-invalid\' : \'\'; ?>" value="<?php echo $image_categorie; ?>"',
-        'invalid_feeback' =>'$image_categorie_err; ?>',]],
+        'invalid_feeback' =>'$image_categorie_err; ?>'],
+    ] ],
 ];
